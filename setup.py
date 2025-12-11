c89caee1b82497338d93ad75d6701f8a98b2d0fc
Golden Dev <miguizin.10@gmail.com>
2025-12-11 13:34:48 -0300

    updated version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme() -> str:
 
 setup(
     name = "golden-utils",
-    version = "0.1.0",
+    version = "0.1.1",
     packages = find_packages(where = "src"),
     package_dir = {"": "src"},
 
