27ba0eb175cf497a990d2c603f618b3f0e56e946
Golden Dev <miguizin.10@gmail.com>
2025-12-11 14:24:21 -0300

    IMPORT FINALLY WORKING CORRECTLY

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme() -> str:
 
 setup(
     name = "golden-utils",
-    version = "0.1.2",
+    version = "0.1.3",
     packages = find_packages(where = "src"),
     package_dir = {"": "src"},
 
